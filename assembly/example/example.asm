d9e024bb7b8f33949fdea7757a3fb2ef340101a2
Ethan Sherman <ser719577@gmail.com>
2026-01-13 16:21:34 -0400

    hello

---
+++ assembly/example/example.asm
@@ -0,0 +1,24 @@
+section .data
+
+  num1 dq 15
+  num2 dq 4
+  result dq 0
+
+section .text
+  global _start
+
+_start:
+  ; load first number
+  mov rax , [num1] 
+
+  add rax , [num2]
+
+  mov rbx, 2
+
+  mul rbx
+
+  mov[result], rax
+
+  mov rax, 60 
+  mov rdi, [result]
+  syscall
