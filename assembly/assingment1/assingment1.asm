66403863d5018e6c47aa0a7e39c54e213958d43b
Ethan Sherman <ser719577@gmail.com>
2026-01-19 11:42:20 -0400

    hello

---
+++ assembly/assingment1/assingment1.asm
@@ -0,0 +1,27 @@
+section .data 
+  dividend dq 50
+  divisor dq 7
+
+section .text
+    global _start
+
+_start:
+  mov rax, [dividend]
+  mov rbx, [divisor]
+  mov rcx, 0
+  mov rdx, rax
+
+loop_start:
+    ; Keep looping while remainder > divisor
+    cmp rdx, rbx          ; compare remainder and divisor
+    jae subtract_step      ; if remainder >= divisor, go do subtraction
+
+    mov rax, rcx
+    mov rdi, rax
+    mov rax, 60
+    syscall
+
+subtract_step:
+    sub rdx, rbx          ; remainder -= divisor
+    inc rcx               ; quotient++
+    jmp loop_start        ; repeat
